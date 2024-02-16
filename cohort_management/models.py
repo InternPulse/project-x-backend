cbbd246dbed09b1b23a4c250b485be9f0b7e3e9a
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-02-16 02:37:07 +0200

    Created models for cohort and internprofile

---
+++ cohort_management/models.py
@@ -0,0 +1,21 @@
+# cohort_management/models.py
+
+from django.db import models
+from user_management.models import User  # Import the User model from user_management app
+from utils.models import BaseModel
+
+class Cohort(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    rules = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class InternProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50)  # Choices [admin, member, ...]
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
--- internpulse_project/settings.py
+++ internpulse_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user_management',
     'notifications',
+    'cohort_management'
 ]
 
 MIDDLEWARE = [
