22f07d4cd679d7558c713d2d4e5f894488d0abcc
Edfolmi <ephraimdanielfolmi@gmail.com>
2024-02-26 21:11:57 +0100

    removed app_name from certificates\urls.py

--- certificates/urls.py
+++ certificates/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import CertificateListCreateAPIView, CertificateDetailAPIView, CertificateIssueBatchAPIView
 
 
-app_name = "certificates"
-
 urlpatterns = [
     path("certificates/", CertificateListCreateAPIView.as_view(), name="certificate-list-create"),
     path("certificates/<int:pk>/", CertificateDetailAPIView.as_view(), name="certificate-detail"),
