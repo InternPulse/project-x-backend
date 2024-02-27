db3fba513059a70b43ccf4d7a62f9886255f4ba1
Edfolmi <ephraimdanielfolmi@gmail.com>
2024-02-27 16:04:40 +0100

    Modified the certificate app feature

--- certificates/admin.py
+++ certificates/admin.py
@@ -5,8 +5,8 @@ from .models import Certificate
 
 # Register your models here.
 class CertificateAdmin(admin.ModelAdmin):
-    list_display = ('id', 'cohort', 'title', 'description', 'image', 'issued_to', 'issue_date', 'created_at', 'updated_at')
-    list_filter = ('issue_date', 'created_at', 'updated_at')
-    search_fields = ('id', 'cohort', 'title', 'description', 'issued_to',)
+    list_display = ('id', 'user', 'cohort', 'intern_name', 'stack', 'issue_date', 'created_at', 'updated_at')
+    list_filter = ('cohort', 'issue_date', 'created_at', 'updated_at')
+    search_fields = ('id', 'user', 'cohort', 'intern_name', 'stack')
 
 admin.site.register(Certificate, CertificateAdmin)
