c67d5ff7cd0357e2dc98facdffd1b538882815ef
Edfolmi <ephraimdanielfolmi@gmail.com>
2024-02-27 16:56:42 +0100

    Added back the MySQL db

--- internpulse_project/settings.py
+++ internpulse_project/settings.py
@@ -106,18 +106,18 @@ WSGI_APPLICATION = "internpulse_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    #  'default': {
-    #      'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': config('DB_NAME'),
-    #      'USER': config('DB_USER'),
-    #     'PASSWORD': config("DB_PASSWORD"),
-    #      'HOST': config('DB_HOST'),
-    #      'PORT': config('DB_PORT'),
-    # }
-    'default' : {
-        'ENGINE' : 'django.db.backends.sqlite3',
-        "NAME" : 'db.sqlite3',
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+         'USER': config('DB_USER'),
+        'PASSWORD': config("DB_PASSWORD"),
+         'HOST': config('DB_HOST'),
+         'PORT': config('DB_PORT'),
     }
+    # 'test' : {
+    #     'ENGINE' : 'django.db.backends.sqlite3',
+    #     "NAME" : 'db.sqlite3',
+    # }
 }
 
 REST_FRAMEWORK = {
