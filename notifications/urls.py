93f19b1ab87fdafbcb3c1c3d1ad89e4b6c3a403a
FestusMike <festusmike98@gmail.com>
2024-02-28 14:17:23 +0100

    Email Templates Added

--- notifications/urls.py
+++ notifications/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import TalentRequestTicketRegistrationView
 
 urlpatterns = [
-    path("talent-request/", TalentRequestTicketRegistrationView.as_view(), name="talent-request",),
+    path(
+        "talent-request/",
+        TalentRequestTicketRegistrationView.as_view(),
+        name="talent-request",
+    ),
 ]
